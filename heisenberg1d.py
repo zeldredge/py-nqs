789e4c532ab0d440eb90a8df6f480a617f2690dc
Zachary Eldredge <zeldredge@gmail.com>
2017-02-16 12:00:14 -0500

    Refactored to be PEP-8 compliant, make the IDE happy, be a little more readable

--- heisenberg1d.py
+++ heisenberg1d.py
@@ -1,36 +1,36 @@
 import numpy as np
 
+
 class Heisenberg1d:
-
-
-    def __init__(self,nspins,jz,pbc = True): #Input args are number of spins, Jz coupling, and whether periodic boundary conditions apply
+    def __init__(self, nspins, jz, pbc=True):
+        # Input args are number of spins, Jz coupling, and whether periodic boundary conditions apply
         self.nspins = nspins
         self.jz = jz
         self.pbc = pbc
         self.minflips = 2
         print("# Using the 1d Heisenberg model with J_z = {}".format(jz))
-        
-    def FindConn(self,state): #Given a state, find all states connected to it by this Hamiltonian
-        #State should be a vector of \pm 1
+
+    def find_conn(self, state):  # Given a state, find all states connected to it by this Hamiltonian
+        # State should be a vector of \pm 1
         mel = np.zeros(1)
-        flipsh = np.array([[0,0]]) #The "0,0" entry is a dummy, and is dealt with differently by nqs  
+        flipsh = np.array([[0, 0]])  # The "0,0" entry is a dummy, and is dealt with differently by nqs
 
-        #First we do the ZZ interaction
-        mel[0] = sum([state[i]*state[i+1] for i in range(self.nspins-1)])
-        if self.pbc: #also check the ends if pbc
-            mel[0] += state[0]*state[-1]
-        mel[0] *= self.jz #Multiply by couplign constant
+        # First we do the ZZ interaction
+        mel[0] = sum([state[i] * state[i + 1] for i in range(self.nspins - 1)])
+        if self.pbc:  # also check the ends if pbc
+            mel[0] += state[0] * state[-1]
+        mel[0] *= self.jz  # Multiply by couplign constant
 
-        #Look for possible spin flips
-        for i in range(self.nspins-1):
-            if state[i] != state[i+1]:
-                mel = np.append(mel,-2)
-                flipsh = np.append(flipsh,[[i,i+1]],axis = 0)
+        # Look for possible spin flips
+        for i in range(self.nspins - 1):
+            if state[i] != state[i + 1]:
+                mel = np.append(mel, [-2])
+                flipsh = np.append(flipsh, [[i, i + 1]], axis=0)
 
         if self.pbc:
             if state[-1] != state[0]:
-                mel = np.append(mel,-2)
-                flipsh = np.append(flipsh,[[-1,0]],axis = 0)  
+                mel = np.append(mel, [-2])
+                flipsh = np.append(flipsh, [[-1, 0]], axis=0)
 
-        return mel,flipsh #So what we have here is a vector of all matrix elements, and then a vector which tells you which states to flip to get that connection
-
+        return mel, flipsh  # So what we have here is a vector of all matrix elements,
+        # and then a vector which tells you which states to flip to get that connection
--- small_main.py
+++ small_main.py
@@ -3,10 +3,10 @@ import nqs
 import heisenberg1d
 import sys
 
-nruns = int(sys.argv[1])
+nruns = 100
 
-wf = nqs.Nqs("../Ground/Heisenberg1d_40_1_1.wf")
+wf = nqs.Nqs("./Ground/Heisenberg1d_40_1_1.wf")
 h = heisenberg1d.Heisenberg1d(40, 1)
 s = sampler.Sampler(wf, h)
 
-s.Run(nruns)
+s.run(nruns)
