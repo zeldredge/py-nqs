Fixed sign errors in the fermion hopping hamiltonian. Doesn't want to train, still -- trying to determine why.

Hamiltonians/fermionhop1d.py
@@ -25,22 +25,23 @@ class FermionHop: # Class implementing Hamiltonian for \sum_(i) c_i+1^dag c_i+ h
                 # in order of increasing i. So the question is what sign accumulates when we commute c^\dag_i+1 c_i
                 # past the string. Well, we need to move it past all c^\dag_k with k > i + 1. The annhilation operator
                 # commutes without complication, the creation operator accumulates a -1 for every one it crosses
-                sign = (-1)**len([x for x in state[i+2:] if x == 1])
+                sign = (-1)**len([x for x in state[:i] if x == 1])
                 mel.append(self.t*sign)
 
-            elif state[i+1] == 1 and state[i] == 1: # or the other hop
-                flips.append(np.array([i + 1, i]))
-                sign = (-1) ** len([x for x in state[i + 1:] if x == 1])
+            elif state[i+1] == 1 and state[i] == -1: # or the other hop
+                flips.append(np.array([i, i+ 1]))
+                sign = (-1) ** len([x for x in state[:i] if x == 1])
                 mel.append(self.t * sign)
 
         if self.pbc: # Periodic boundaries?
             if state[-1] == 1 and state[0] == -1:
-                flips.append(np.array([-1, 0]))
-                mel.append(self.t) # In this case, c_0 commutes through and c^dag_N is already at the end -- no sign
+                flips.append(np.array([0, -1]))
+                sign = (-1) ** len([x for x in state if x == 1])
+                mel.append(self.t*sign) # In this case, c_0 commutes through and c^dag_N is already at the end -- no sign
 
-            elif state[-1] == 1 and state[0] == -1:
+            elif state[-1] == -1 and state[0] == 1:
                 flips.append(np.array([0, - 1]))
-                sign = (-1)**len([x for x in state if x == 1])
-                mel.append(self.t*sign)  # In this case, c^\dag_0 commutes through and c_N is already at the end
+
+                mel.append(self.t)  # In this case, c^\dag_0 commutes through and c_N is already at the end
 
         return mel, flips

Scripts/ti_train.py
@@ -12,7 +12,7 @@ import fermionhop1d
 start = time.time()
 nruns = 1000
 nspins = 10
-nsteps = 1000
+nsteps = 100
 m = True
 
 wf = nqs.NqsTI(nspins, 1)  # A translation invariant NQS instance
@@ -23,11 +23,11 @@ wf.b = 0.1*np.random.random(wf.b.shape) + 0j
 
 #h = ising1d.Ising1d(40,0.5)
 #h = heisenberg1d.Heisenberg1d(10,1)
-h = fermionhop1d.FermionHop(nspins,-1)
+h = fermionhop1d.FermionHop(nspins,-11)
 #base_array = np.concatenate(
                 #(np.ones(int(nspins/2)), -1 * np.ones(int(nspins/2))))  # make an array of half 1, half -1
 base_array = -1*np.ones(nspins)
-base_array[0:nspins//2 + 1] *= -1
+base_array[0:nspins//2 ] *= -1
 state = np.random.permutation(base_array)  # return a random permutation of the half 1, half-1 array
 wf.init_lt(state)
 

trainer.py
@@ -49,7 +49,7 @@ class Trainer:
 
         elocals = np.array([i[0] for i in results])
         deriv_vectors = np.array([i[1] for i in results])
-
+        states = np.array([i[2] for i in results])
         # Now that we have all the data from sampling let's run our statistics
         # cov = self.get_covariance(deriv_vectors)
         cov_operator = LinearOperator((self.nvar, self.nvar), dtype=complex,
@@ -293,4 +293,4 @@ def build_trainer(wf, h, reg_list=(100, 0.9, 1e-4), cores=1, m = True):
 def get_sample(sampler, trainer):
     for i in range(sampler.nspins):
         sampler.move()
-    return trainer.get_elocal(sampler.state, sampler.wf), trainer.get_deriv_vector(sampler.state, sampler.wf)
+    return trainer.get_elocal(sampler.state, sampler.wf), trainer.get_deriv_vector(sampler.state, sampler.wf), sampler.state